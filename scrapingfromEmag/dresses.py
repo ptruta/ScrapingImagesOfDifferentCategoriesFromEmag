d9655ad0dd55494a38fb8ef7fd67262578931b97
Truta Patricia <truta_georgiana@yahoo.com>
2021-12-14 03:45:17 +0200

    Scraping images with Python from Emag
    Scraping images with Python from Emag script

---
+++ scrapingfromEmag/dresses.py
@@ -0,0 +1,43 @@
+# Importing the required libraries
+import os
+
+from IPython.display import display
+import requests
+import pandas as pd
+from bs4 import BeautifulSoup
+from tabulate import tabulate
+
+
+def create_dir():
+    # Directory
+    dir_ = "Dresses"
+    # Parent Directory path
+    parent_dir = os.path.dirname(os.path.realpath(__file__))
+    # Path
+    path = os.path.join(parent_dir, dir_)
+    # os_mkdir(path)
+    return path
+
+
+# for idx in excelData.index:
+
+# url = excelData['URL'][idx]
+url = 'https://www.emag.ro/search/rochii?ref=effective_search'
+
+# # Downloading contents of the web page
+data = requests.get(url).text
+soup = BeautifulSoup(data, 'html.parser')
+
+i=0
+for item in soup.find_all('img'):
+    # print(item['src'])
+    headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/60.0",
+               "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+               "Accept-Language": "en-US,en;q=0.9"
+               }
+
+    img_data = requests.get(url=item['src'], headers=headers).content
+    with open('Dresses' + "/" + 'image_name' + i.__str__()+ '.png', 'wb') as handler:
+        print(img_data)
+        handler.write(img_data)
+    i=i+1
